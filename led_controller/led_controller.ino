7d09f2df5a20b9c22c3d8117f1567f9c546814f7
Raffaello Bonghi <raffaello@rnext.it>
2017-09-01 22:15:28 +0200

    Added subscriber definition. Clean and fix.

--- led_controller/led_controller.ino
+++ led_controller/led_controller.ino
@@ -1,20 +1,9 @@
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-
-#include <ros.h>
-#include <std_msgs/String.h>
-
-ros::NodeHandle  nh;
-
-std_msgs::String str_msg;
-ros::Publisher chatter("chatter", &str_msg);
-
-char hello[13] = "hello world!";
+// -------------------------//
+//       LED DEFINITION     //
+// -------------------------//
 
 // Led definition
 #define PIN_LEFT            2
@@ -30,6 +19,28 @@ char hello[13] = "hello world!";
 
 #define INTENSITY          50
 
+// -------------------------//
+
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+
+// TODO To remove
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+ros::NodeHandle  nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
+
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
@@ -40,15 +51,43 @@ Adafruit_NeoPixel pixels_right = Adafruit_NeoPixel(NUMPIXELS, PIN_RIGHT, NEO_GRB
 uint32_t color = pixels_left.Color(255, 255, 255);
 uint32_t led_off = pixels_left.Color(0,0,0);
 
+// LED colors sequence
+uint32_t colors[] = {
+  pixels_left.Color(0, INTENSITY, 0),
+  pixels_left.Color(INTENSITY, 0, 0),
+  pixels_left.Color(0, 0, INTENSITY),
+  pixels_left.Color(INTENSITY, INTENSITY, 0),
+  pixels_left.Color(INTENSITY, 0, INTENSITY),
+  pixels_left.Color(0, INTENSITY, INTENSITY),
+  pixels_left.Color(INTENSITY, INTENSITY, INTENSITY),
+  pixels_left.Color(0, 0, 0),
+  };
+// Size of LED colors
+int size_colors = 0;
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+  digitalWrite(LED_BUILTIN, HIGH-digitalRead(LED_BUILTIN));   // blink the led
+}
+
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+
 void setup() {
+  // Initialization LED builtin
+  pinMode(LED_BUILTIN, OUTPUT);
   // Initialization Green strip LED
   pinMode(LED_GREEN, OUTPUT);
   // Set low LED
   digitalWrite(LED_GREEN, LOW);
   pixels_left.begin(); // This initializes the NeoPixel library.
   pixels_right.begin(); // This initializes the NeoPixel library.
-
+  // Evaluate size of colors led array
+  size_colors = sizeof(colors)/sizeof(uint32_t);
+  
+  // Initialization ROS node
   nh.initNode();
+  // Initialization subscriber
+  nh.subscribe(sub);
+  // TODO to REMOVE
   nh.advertise(chatter);
 }
 
@@ -57,14 +96,11 @@ void loop() {
   str_msg.data = hello;
   chatter.publish( &str_msg );
   nh.spinOnce();
-  
-  led_swipe(pixels_left.Color(0, INTENSITY, 0), DELAY_VAL, 0, NUMPIXELS);
-  led_swipe(pixels_left.Color(INTENSITY, 0, 0), DELAY_VAL, 0, NUMPIXELS);
-  led_swipe(pixels_left.Color(0, 0, INTENSITY), DELAY_VAL, 0, NUMPIXELS);
-  led_swipe(pixels_left.Color(INTENSITY, INTENSITY, 0), DELAY_VAL, 0, NUMPIXELS);
-  led_swipe(pixels_left.Color(INTENSITY, 0, INTENSITY), DELAY_VAL, 0, NUMPIXELS);
-  led_swipe(pixels_left.Color(0, INTENSITY, INTENSITY), DELAY_VAL, 0, NUMPIXELS);
-  led_swipe(pixels_left.Color(INTENSITY, INTENSITY, INTENSITY), DELAY_VAL, 0, NUMPIXELS);
+
+  // Color swipe led loop
+  for(int i = 0; i < size_colors; ++i) {
+    led_swipe(colors[i], DELAY_VAL, 0, NUMPIXELS);
+  }
 }
 
 void led_swipe(uint32_t color, uint32_t d, uint32_t sta, uint32_t sto) {
